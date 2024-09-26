035dc49f7b61a1be47c831551a6ab93eb7b72a72
Humza Shahid <humzasaur@gmail.com>
2024-09-27 00:08:57 +0100

    integrate mock_db and alert_controller with skeleton

--- user_monitoring/api.py
+++ user_monitoring/api.py
@@ -1,10 +1,38 @@
-from flask import Blueprint, current_app
+from flask import Blueprint, request, current_app
+from user_monitoring.mock_db import MockDb
+from user_monitoring.alert_controller import AlertController
 
 
 api = Blueprint("api", __name__)
 
+db = MockDb()
 
 @api.post("/event")
 def handle_user_event() -> dict:
     current_app.logger.info("Handling user event")
-    return {}
+
+    # Check for validation errors
+    validationErrors = AlertController.get_validation_errors(request.json)
+
+    # Return response containing validation errors
+    # if there is at least one error
+    if (len(validationErrors) > 0):
+        return { "errors": validationErrors }
+
+    # Save transaction to mock db if there are not validation errors
+    db.add_transaction(request.json)
+
+    # Get transaction history from user's ID
+    userId = request.json["user_id"]
+    userTransactions = db.get_user_transactions(userId)
+
+    # Get alerts from user's transactions
+    alerts = AlertController.get_user_alerts(userTransactions)
+    # If there is at least one alert returned
+    hasAlert = len(alerts) > 0
+
+    return {
+        "alert": hasAlert,
+        "alert_codes": alerts,
+        "user_id": userId
+    }
